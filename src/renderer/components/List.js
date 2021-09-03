1030469e55338555f98ae32c0e8ad70e5f6dca4a
dehmer <horst.dehmer@syncpoint.io>
2021-09-03 12:21:56 +0200

    react-cool-virtual: experimenting (WIP).

--- src/renderer/components/List.js
+++ src/renderer/components/List.js
@@ -1,53 +1,54 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
-/**
- *
- */
-const scrollIntoView = (refs, index, behavior) =>
-  refs[index] &&
-  refs[index].current &&
-  refs[index].current.scrollIntoView({
-    behavior,
-    block: 'nearest'
-  })
+import useVirtual from 'react-cool-virtual'
 
 
 /**
  * Abstract list. Mainly obsessed with scrolling.
  */
 const List = props => {
-  const { child, focusId, selected } = props
-  const cardrefs = props.entries.map(_ => React.createRef())
+  const { child, entries, focusId, selected } = props
+  const { outerRef, innerRef, items, scrollToItem } = useVirtual({
+    itemCount: entries.length,
+    itemSize: 200
+  })
 
   React.useEffect(() => {
     if (props.scroll === 'none') return
-    scrollIntoView(cardrefs, props.focusIndex, props.scroll)
-  }, [cardrefs, props.focusIndex, props.scroll])
+    if (props.focusIndex === -1) return
+    scrollToItem({ index: props.focusIndex, smooth: false })
+  }, [scrollToItem, props.focusIndex, props.scroll])
 
   const handleKeyDown = event => {
     const { key } = event
     if (key === ' ') event.preventDefault()
   }
 
-  const card = (entry, index) => {
+  const card = ({ index, measureRef }) => {
+    const entry = entries[index]
+    if (index >= entries.length) {
+      console.warn('<List/> overshooting', `${index}/${entries.length}`)
+      return null
+    }
+
     return child({
       entry,
       id: entry.id,
       focused: focusId === entry.id,
       selected: selected.includes(entry.id),
-      ref: cardrefs[index]
+      ref: measureRef
     })
   }
 
-  const list = props.entries.length
-    ? props.entries.map(card)
+  const list = items.length
+    ? items.map(card)
     : null
 
   return (
-    <div className='list-container'>
+    <div className='list-container' ref={outerRef}>
       <div
         className='list'
+        ref={innerRef}
         tabIndex={0}
         onKeyDown={handleKeyDown}
       >
@@ -70,5 +71,6 @@ List.whyDidYouRender = true
 
 const ListMemo = React.memo(List)
 ListMemo.whyDidYouRender = true
-
 export { ListMemo as List }
+
+// export { List }
