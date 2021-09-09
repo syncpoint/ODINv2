89da25b979a06aca045a0cd1919ff7918d112021
dehmer <horst.dehmer@syncpoint.io>
2021-09-09 11:10:57 +0200

    fixed undo/redo unit test.

--- test/renderer/Undo-test.js
+++ test/renderer/Undo-test.js
@@ -3,7 +3,7 @@ import { Undo } from '../../src/renderer/Undo'
 
 const command = (queue, name, inverse = false) => {
   return {
-    apply: () => queue.push(`${name} - ${inverse ? 'inverse' : 'forward'}`),
+    apply: () => queue.push(`${name}/${inverse ? 'inverse' : 'forward'}`),
     inverse: () => command(queue, name, !inverse)
   }
 }
@@ -26,7 +26,7 @@ describe('Undo', function () {
     await undo.apply(command(queue, 'A'))
     assert.strictEqual(undo.canUndo(), true, 'can undo')
     assert.strictEqual(undo.canRedo(), false, 'cannot redo')
-    assert.deepStrictEqual(queue, ['A - forward'])
+    assert.deepStrictEqual(queue, ['A/forward'])
   })
 
   it('undo', async function () {
@@ -36,7 +36,7 @@ describe('Undo', function () {
     await undo.undo()
     assert.strictEqual(undo.canUndo(), false, 'cannot undo')
     assert.strictEqual(undo.canRedo(), true, 'can redo')
-    assert.deepStrictEqual(queue, ['A - forward', 'A - inverse'])
+    assert.deepStrictEqual(queue, ['A/forward', 'A/inverse'])
   })
 
   it('redo', async function () {
@@ -47,32 +47,36 @@ describe('Undo', function () {
     await undo.undo()
     assert.strictEqual(undo.canRedo(), true, 'can redo')
     await undo.redo()
-    assert.deepStrictEqual(queue, ['A - forward', 'A - inverse', 'A - forward'])
+    assert.deepStrictEqual(queue, ['A/forward', 'A/inverse', 'A/forward'])
   })
 
   it('undo/redo', async function () {
     const undo = new Undo()
     const queue = []
-    await undo.apply(command(queue, 'A')) // 'A - forward'
-    await undo.apply(command(queue, 'B')) // 'B - forward'
-    await undo.undo() // 'B - inverse'
-    await undo.apply(command(queue, 'C')) // 'C - forward'
-    await undo.redo() // 'B - forward'
-    await undo.undo() // 'B - inverse'
-    await undo.undo() // 'C - inverse'
-    await undo.undo() // 'A - inverse'
+    await undo.apply(command(queue, 'A')) // 'A/forward'
+    await undo.apply(command(queue, 'B')) // 'B/forward'
+    await undo.undo() // 'B/inverse'
+
+    // Note: Discards redo B/forward:
+    await undo.apply(command(queue, 'C')) // 'C/forward'
+    await undo.redo() // noop
+    await undo.undo() // 'C/inverse'
+    await undo.redo() // 'C/forward'
+    await undo.redo() // 'C/forward'
+    await undo.undo() // 'B/inverse'
+    await undo.undo() // 'A/inverse'
 
     assert.strictEqual(undo.canUndo(), false, 'cannot undo')
     assert.strictEqual(undo.canRedo(), true, 'can redo')
     assert.deepStrictEqual(queue, [
-      'A - forward',
-      'B - forward',
-      'B - inverse',
-      'C - forward',
-      'B - forward',
-      'B - inverse',
-      'C - inverse',
-      'A - inverse'
+      'A/forward',
+      'B/forward',
+      'B/inverse',
+      'C/forward',
+      'C/inverse',
+      'C/forward',
+      'C/inverse',
+      'A/inverse'
     ])
   })
 })
