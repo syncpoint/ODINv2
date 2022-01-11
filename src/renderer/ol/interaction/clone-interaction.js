90a6160e20d312b1185d7ceb8ea78b50c0d37d13
dehmer <horst.dehmer@syncpoint.io>
2022-01-11 12:32:54 +0100

    separated translate and clone interactions.

---
+++ src/renderer/ol/interaction/clone-interaction.js
@@ -0,0 +1,118 @@
+import uuid from 'uuid-random'
+import { Translate } from 'ol/interaction'
+import { MAC } from 'ol/has'
+import { writeGeometryObject } from '../../store/format'
+import * as ids from '../../ids'
+
+
+/**
+ *
+ */
+export default (options, select) => {
+  const { store, featureSource, hitTolerance, selection } = options
+
+  // Has cloning operation started?
+  let cloning = false
+
+  let cancelled = false
+
+  // snapshot :: [ol/Feature]
+  // Current feature list is necessary for cancel.
+  let snapshot = []
+
+  // clones :: [ol/Feature]
+  let clones = []
+
+  const interaction = new Translate({
+    hitTolerance,
+    features: select.getFeatures(),
+
+    // Both platform modifier key and shift key must be pressed,
+    // but no other modifier key (alt).
+    condition: event => {
+      const originalEvent = event.originalEvent
+      return (
+        !originalEvent.altKey &&
+        (MAC ? originalEvent.metaKey : originalEvent.ctrlKey) &&
+        originalEvent.shiftKey
+      )
+    }
+  })
+
+  interaction.on('translatestart', event => {
+    console.log('[CLONE] translatestart', event)
+
+    cloning = true
+    cancelled = false
+    const map = interaction.getMap()
+    const target = map.getViewport()
+    target.style.cursor = 'copy'
+
+    snapshot = [...event.features.getArray()]
+
+    // Clone features with new identities.
+
+    clones = snapshot.map(feature => {
+      const layerUUID = ids.layerUUID(feature.getId())
+      const clone = feature.clone()
+      const id = `feature:${layerUUID}/${uuid()}`
+      clone.setId(id)
+      return clone
+    })
+
+    // Add clones to source as placeholders.
+    // Placeholders must be removed before adding to store,
+    // because re-adding them to the source will result in a major
+    // (feature) identity crisis.
+    featureSource.addFeatures(clones)
+  })
+
+  interaction.on('translateend', async event => {
+    console.log('[CLONE] translateend', event)
+
+    cloning = false
+    const map = interaction.getMap()
+    const target = map.getViewport()
+    target.style.cursor = null
+
+    if (cancelled) return
+
+    // Get complete properties set for all features.
+    // Note: ol/Feature does not carry all persisted data.
+    const features = event.features.getArray()
+    const ids = features.map(feature => feature.getId())
+    const properties = await store.select(ids)
+
+    // Swap geometries: feature <-> clone.
+    features.forEach((feature, index) => {
+      const geometry = feature.getGeometry()
+      feature.setGeometry(clones[index].getGeometry())
+      clones[index].setGeometry(geometry)
+    })
+
+    // Prepare new JSON features to put into store:
+    const json = clones.map((clone, index) => {
+      const geometry = writeGeometryObject(clone.getGeometry())
+      return { ...properties[index], id: clone.getId(), geometry }
+    })
+
+    // Important: Remove clones from source before adding to store.
+    clones.forEach(clone => featureSource.removeFeature(clone))
+    await store.insertFeatures(json)
+    selection.set(clones.map(clone => clone.getId()))
+  })
+
+  // Cancel operation on selection change:
+  selection.on('selection', () => {
+    if (!cloning) return
+
+    cancelled = true
+
+    clones.forEach((clone, index) => {
+      snapshot[index].setGeometry(clone.getGeometry().clone())
+      featureSource.removeFeature(clone)
+    })
+  })
+
+  return interaction
+}
--- src/renderer/ol/interaction/translate-interaction.js
+++ src/renderer/ol/interaction/translate-interaction.js
@@ -1,167 +1,36 @@
-import util from 'util'
-import uuid from 'uuid-random'
-import Emitter from '../../../shared/emitter'
 import { Translate } from 'ol/interaction'
-import { cmdOrCtrl } from '../../platform'
-import * as ids from '../../ids'
-import { writeGeometryObject } from '../../store/format'
-
-/**
- * Track whether CMD/CTRL is currently pressed.
- * Use in translate interaction for cloning features.
- */
-const CmdOrCtrlTracker = function () {
-  Emitter.call(this)
-  this.handler_ = event => {
-    const current = this.cmdOrCtrl && event.shiftKey
-    const value = cmdOrCtrl(event)
-    if (current !== value) this.emit('update', { cmdOrCtrl: value })
-    this.cmdOrCtrl = value
-  }
-
-  document.addEventListener('keydown', this.handler_)
-  document.addEventListener('keyup', this.handler_)
-}
-
-util.inherits(CmdOrCtrlTracker, Emitter)
-
-/* Note: Currently not used. */
-CmdOrCtrlTracker.prototype.dispose = function () {
-  document.removeEventListener('keydown', this.handler_)
-  document.removeEventListener('keyup', this.handler_)
-}
-
-const cmdOrCtrlTracker = new CmdOrCtrlTracker()
-
+import { writeFeatureCollection } from '../../store/format'
+import { noModifierKeys, shiftKeyOnly } from 'ol/events/condition'
 
 /**
  *
  */
 export default (options, select) => {
-  const { store, featureSource, hitTolerance, selection } = options
+  const { store, hitTolerance } = options
+
+  // snapshot :: GeoJSON/FeatureCollection
+  let snapshot = {}
 
   const interaction = new Translate({
     hitTolerance,
-    features: select.getFeatures()
+    features: select.getFeatures(),
+    condition: event => noModifierKeys(event) || shiftKeyOnly(event)
   })
 
-  const CLONES = 'clones'
-  const CLONING = 'cloning'
-  const CANCELLED = 'cancelled'
-  const UPDATE_CURSOR = 'updateCursor'
-  const FEATURES = 'features'
-
-  const silent = true
-  const set = (key, value) => {
-    interaction.set(key, value, silent)
-    return value
-  }
-
-  const unset = key => {
-    const value = interaction.get(key)
-    interaction.unset(key, silent)
-    return value
-  }
-
   interaction.on('translatestart', event => {
-    set(CANCELLED, false)
-
-    const updateCursor = set(UPDATE_CURSOR, cursor => {
-      const map = interaction.getMap()
-      const target = map.getViewport()
-      target.style.cursor = cursor
-    })
-
-    // Clone features with new identities.
-    // New identities are only necessary for clone interaction.
-    // Simple translate ignores feature identities and
-    // only takes geometries by position in array.
-
-    const features = set(FEATURES, [...event.features.getArray()])
-    const clones = set(CLONES, features.map(feature => {
-      const layerUUID = ids.layerUUID(feature.getId())
-      const clone = feature.clone()
-      const id = `feature:${layerUUID}/${uuid()}`
-      clone.setId(id)
-      return clone
-    }))
-
-    const cloning = set(CLONING, cmdOrCtrlTracker.cmdOrCtrl)
-    if (cloning) {
-      updateCursor('copy')
-
-      // Add clones to source as placeholders.
-      // Placeholders must be removed before adding to store,
-      // because re-adding them to the source will result in a major
-      // (feature) identity crisis.
-      featureSource.addFeatures(clones)
-    }
+    const features = event.features.getArray()
+    snapshot = writeFeatureCollection(features)
   })
 
   interaction.on('translateend', async event => {
-    const cloning = unset(CLONING)
-    const clones = unset(CLONES)
-    const cancelled = unset(CANCELLED)
-    const updateCursor = unset(UPDATE_CURSOR)
-    unset(FEATURES)
+    const { features } = writeFeatureCollection(event.features.getArray())
+    const [newValues, oldValues] = features.reduce((acc, feature, index) => {
+      acc[0].push(feature)
+      acc[1].push(snapshot.features[index])
+      return acc
+    }, ([[], []]))
 
-    if (cancelled) return
-
-    updateCursor(null)
-    const features = event.features.getArray()
-
-
-    if (cloning) {
-
-      // Get complete properties set for all features:
-      const ids = features.map(feature => feature.getId())
-      const properties = await store.select(ids)
-
-      // Swap geometries: feature <-> clone.
-      features.forEach((feature, index) => {
-        const geometry = feature.getGeometry()
-        feature.setGeometry(clones[index].getGeometry())
-        clones[index].setGeometry(geometry)
-      })
-
-      // Prepare new JSON features to put into store:
-      const json = clones.map((clone, index) => {
-        const geometry = writeGeometryObject(clone.getGeometry())
-        return { ...properties[index], id: clone.getId(), geometry }
-      })
-
-      // Important: Remove clones from source before adding to store.
-      clones.forEach(clone => featureSource.removeFeature(clone))
-      await store.insertFeatures(json)
-      selection.set(clones.map(clone => clone.getId()))
-    } else {
-
-      // geometries :: { id -> [new, old] }
-      const geometries = features.reduce((acc, feature, index) => {
-        acc[feature.getId()] = [
-          writeGeometryObject(feature.getGeometry()), // new geometry
-          writeGeometryObject(clones[index].getGeometry()) // old geometry
-        ]
-        return acc
-      }, {})
-
-      store.updateGeometries(geometries)
-    }
-  })
-
-  // Cancel on deselect:
-  selection.on('selection', () => {
-    if (!interaction.get(CLONING)) return
-
-    set(CANCELLED, true)
-    interaction.get(UPDATE_CURSOR)(null)
-    const clones = interaction.get(CLONES)
-    const features = interaction.get(FEATURES)
-
-    clones.forEach((clone, index) => {
-      features[index].setGeometry(clone.getGeometry().clone())
-      featureSource.removeFeature(clone)
-    })
+    store.update(newValues, oldValues)
   })
 
   return interaction
