e070a94a1059e9fc9b16083868d6c5bb649dc16a
dehmer <horst.dehmer@syncpoint.io>
2022-01-24 20:05:08 +0100

    properties: circle geometry.

--- src/renderer/components/properties/Radius.js
+++ src/renderer/components/properties/Radius.js
@@ -1,17 +1,35 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
 import textProperty from './textProperty'
+import { readGeometry, writeGeometryObject } from '../../model/geometry'
+import * as geom from './geometries'
 
 const TextProperty = textProperty({
   label: 'Radius',
-  get: feature => feature.properties.am ? feature.properties.am : null,
-  set: value => feature => ({
-    ...feature,
-    properties: {
-      ...feature.properties,
-      am: value
+  get: feature => {
+    const { read, write, ...current } = feature
+    const olGeometry = readGeometry(current.geometry)
+    const jtsGeometry = read(olGeometry)
+    const { am } = geom.circleProperties(jtsGeometry)
+    return am
+  },
+  set: value => feature => {
+    const { read, write, ...current } = feature
+    const olGeometry = readGeometry(current.geometry)
+    const jtsGeometry = read(olGeometry)
+    const properties = geom.circleProperties(jtsGeometry)
+    properties.am = value
+    const circle = geom.circle(jtsGeometry, properties)
+
+    return {
+      ...feature,
+      geometry: writeGeometryObject(write(circle)),
+      properties: {
+        ...feature.properties,
+        am: value
+      }
     }
-  })
+  }
 })
 
 export default props => <TextProperty {...props}/>
--- src/renderer/components/properties/geometries.js
+++ src/renderer/components/properties/geometries.js
@@ -17,16 +17,25 @@ export const rectangleProperties = geometry => {
   }
 }
 
+export const circleProperties = geometry => {
+  const coordinates = TS.coordinates(geometry)
+  const segment01 = TS.segment(coordinates[0], coordinates[1])
+
+  return {
+    an: Math.round(RAD2DEG * TS.normalizePositive(segment01.angle())),
+    am: Math.round(segment01.getLength())
+  }
+}
+
 const unitSquare = TS.polygon([
   [-1, 1], [1, 1], [1, -1], [-1, -1], [-1, 1]
 ].map(TS.coordinate))
 
 /**
  * rectangle :: JTS/Geometry -> {k: v} -> JTS/Geometry
+ * FIXME: duplicate code: hooks-rectangle.js
  */
 export const rectangle = (geometry, { am, am1, an }) => {
-  // FIXME: duplicate code: hooks-rectangle.js
-
   const linearRing = geometry.getExteriorRing()
   const center = TS.centroid(linearRing)
   const scale = TS.AffineTransformation.scaleInstance(am / 2, am1 / 2)
@@ -39,4 +48,14 @@ export const rectangle = (geometry, { am, am1, an }) => {
   matrix.compose(translate)
 
   return matrix.transform(unitSquare)
+}
+
+/**
+ * circle :: JTS/Geometry -> {k: v} -> JTS/Geometry
+ */
+export const circle = (geometry, { am, an }) => {
+  const [center] = TS.coordinates(geometry)
+  const point = TS.projectCoordinate(center)([DEG2RAD * an, am])
+  const circle = TS.multiPoint([center, point].map(TS.point))
+  return circle
 }
