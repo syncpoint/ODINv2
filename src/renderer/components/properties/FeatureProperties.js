31cdf12c8a63d678c61f0f035541e715a385b2c7
dehmer <horst.dehmer@syncpoint.io>
2022-01-26 11:57:18 +0100

    properties: removed tabs.

--- src/renderer/components/properties/FeatureProperties.js
+++ src/renderer/components/properties/FeatureProperties.js
@@ -1,7 +1,6 @@
 import * as R from 'ramda'
 import React from 'react'
-import { Tabs } from 'antd'
-import { useServices, useMemento } from '../hooks'
+import { useServices } from '../hooks'
 import * as MILSTD from '../../symbology/2525c'
 import GridCols2 from './GridCols2'
 import MarginTop3 from './MarginTop3'
@@ -12,8 +11,6 @@ import ActivityProperties from './ActivityProperties'
 import GraphicsProperties from './GraphicsProperties'
 import PointProperties from './PointProperties'
 import { isFeatureId } from '../../ids'
-
-const { TabPane } = Tabs
 
 const classes = {
   UNIT: props => <UnitProperties {...props}/>,
@@ -49,7 +46,6 @@ export const FeatureProperties = () => {
   const { selection, store } = useServices()
   const [state, dispatch] = React.useReducer(reducer, {})
   const [className, setClassName] = React.useState(null)
-  const propertiesMemento = useMemento('ui.properties', { tab: 'properties' })
 
   React.useEffect(() => {
     selection.on('selection', async () => {
@@ -77,28 +73,13 @@ export const FeatureProperties = () => {
     // No cleanup necessary; components listenes forever.
   }, [selection, store])
 
-  const handleTabChange = key => {
-    const { value } = propertiesMemento
-    propertiesMemento.put({ ...value, tab: key })
-  }
-
   const tabs = properties => (
     <div className='panel-right panel'>
       <div className='panel-inset'>
-        { /* TODO: remember last active tab */ }
-        <Tabs defaultActiveKey={propertiesMemento.value.tab} onChange={ handleTabChange }>
-          <TabPane tab='Properties' key='properties'>
-            {/* <Panel> */}
-              <GridCols2>
-                <MarginTop3/>
-                { properties({ state })}
-              </GridCols2>
-            {/* </Panel> */}
-          </TabPane>
-          <TabPane tab='Styles' key='styles'>
-            Styles...
-          </TabPane>
-        </Tabs>
+        <GridCols2>
+          <MarginTop3/>
+          { properties({ state })}
+        </GridCols2>
       </div>
     </div>
   )
