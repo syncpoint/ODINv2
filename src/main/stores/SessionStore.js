9615e1e89ab93f2a3e25eeb844b8e68984dcd7e3
dehmer <horst.dehmer@syncpoint.io>
2022-06-22 13:59:10 +0200

    session store: fixed issue with undefined key.

--- src/main/stores/SessionStore.js
+++ src/main/stores/SessionStore.js
@@ -1,4 +1,5 @@
 import * as R from 'ramda'
+import * as L from '../../shared/level'
 
 const SESSION = 'session:'
 const RECENT = 'recent:'
@@ -12,30 +13,30 @@ export const SessionStore = function (db) {
 
 SessionStore.prototype.addProject = async function (key) {
   const projects = session => R.uniq([...session.projects || [], key])
-  const session = await this.db.get(SESSION, { projects: [] })
+  const session = await L.get(this.db, SESSION, { projects: [] })
   await this.db.put(SESSION, { ...session, projects: projects(session) })
 }
 
 SessionStore.prototype.removeProject = async function (key) {
   const projects = session => (session.projects || []).filter(x => x !== key)
-  const session = await this.db.get(SESSION, { projects: [] })
+  const session = await L.get(this.db, SESSION, { projects: [] })
   await this.db.put(SESSION, { ...session, projects: projects(session) })
 }
 
 SessionStore.prototype.getProjects = async function () {
-  const { projects } = await this.db.get(SESSION, { projects: [] })
+  const { projects } = await L.get(this.db, SESSION, { projects: [] })
   return projects
 }
 
 SessionStore.prototype.addRecent = async function (key, name) {
-  const recent = await this.db.get(RECENT, [])
+  const recent = await L.get(this.db, RECENT, [])
   const update = recent.reverse().filter(({ key: x }) => x !== key)
   update.push({ key, name })
   return this.db.put(RECENT, R.take(7, update.reverse()))
 }
 
 SessionStore.prototype.getRecent = async function () {
-  return this.db.get(RECENT, [])
+  return L.get(this.db, RECENT, [])
 }
 
 SessionStore.prototype.clearRecent = function () {
