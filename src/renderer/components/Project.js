af2d79bebc8c50844310e6a0a2aa3ebc4b096a08
dehmer <horst.dehmer@syncpoint.io>
2022-08-12 12:56:35 +0200

    playing with timc1/kbar (WIP).

--- src/renderer/components/Project.js
+++ src/renderer/components/Project.js
@@ -1,12 +1,41 @@
+import * as R from 'ramda'
 import React from 'react'
+import Icon from '@mdi/react'
+import * as mdi from '@mdi/js'
+import uuid from 'uuid-random'
 import { Map } from './map/Map'
 import { Properties } from './properties/Properties'
 import { CommandPalette } from './commandpalette/CommandPalette'
 import { Sidebar } from './sidebar/Sidebar'
 import { Toolbar } from './Toolbar'
+import KBar from './kbar/kbar'
 import { OSD } from './OSD'
 import { useServices, useMemento } from './hooks'
+import * as MILSTD from '../symbology/2525c'
+import { url } from '../symbology/symbol'
 import './Project.css'
+import './kbar/kbar.scss'
+
+function setCharAt (s, i, c) {
+  if (i > s.length - 1) return s
+  return s.substring(0, i) + c + s.substring(i + 1)
+}
+
+const actions = Object.entries(MILSTD.index)
+  .filter(([key, descriptor]) => descriptor?.geometry?.type === 'Point')
+  .map(([key, descriptor]) => {
+    const { hierarchy } = descriptor
+    const sidc = setCharAt(setCharAt(key, 1, 'F'), 3, 'P')
+    const keywords = R.uniq(hierarchy.flatMap(s => s.split(' ')))
+    return {
+      id: key,
+      name: R.last(hierarchy),
+      keywords,
+      icon: url(sidc),
+      perform: () => console.log('perform', sidc),
+      dryRun: () => console.log('dryRun', sidc)
+    }
+  })
 
 const handlers = {
   palette: (state, palette) => ({ ...state, palette })
@@ -16,6 +45,51 @@ const reducer = (state, event) => {
   const handler = handlers[event.type]
   if (handler) return handler(state, event)
   else return state
+}
+
+const Results = () => {
+  const { results: matches } = KBar.useMatches()
+  const onRender = ({ item, active }) => {
+    if (active) item.dryRun()
+
+    const icon = path => <Icon key={uuid()} className='ec35-key' path={mdi[path]}></Icon>
+    const span = token => <span key={uuid()} className='ec35-key'>{token}</span>
+    const separator = () => <span key={uuid()}>&nbsp;&nbsp;</span>
+
+    const avatar = item.icon
+      ? <img src={item.icon} className='ec35-image'/>
+      : item.shortcut
+        ? item.shortcut[0].split(/( )/)
+          .flatMap(token => token.split('+'))
+          .map(token => {
+            if (token === '$mod') return icon('mdiAppleKeyboardCommand')
+            else if (token === 'Control') return icon('mdiAppleKeyboardControl')
+            else if (token === 'Shift') return icon('mdiAppleKeyboardShift')
+            else if (token === 'Option') return icon('mdiAppleKeyboardOption')
+            else if (/^[A-Z]$/i.test(token)) return span(token)
+            else if (token === ' ') return separator()
+            else return token
+          })
+        : null
+
+    return (
+      <div className={active ? 'ec35-item ec35-item--active' : 'ec35-item'}>
+        <div className='ec35-item__title'>
+          {item.name}
+        </div>
+        <div className='ec35-item__avatar'>
+          {avatar}
+        </div>
+      </div>
+    )
+  }
+
+  return <KBar.Results items={matches} onRender={onRender}/>
+}
+
+const Logger = () => {
+  const state = KBar.useKBar(state => state)
+  console.log(state)
 }
 
 /**
@@ -80,6 +154,17 @@ export const Project = () => {
           <OSD/>
           <Properties/>
         </div>
+        <KBar.Provider actions={actions}>
+          <KBar.Portal>
+            <KBar.Positioner className='ec35-positioner'>
+              <KBar.Animator className='ec35-animator'>
+                <KBar.Search className='ec35-search'/>
+                <Results/>
+                <Logger/>
+              </KBar.Animator>
+            </KBar.Positioner>
+          </KBar.Portal>
+        </KBar.Provider>
       </div>
     </div>
   )
---
+++ src/renderer/components/kbar/kbar.js
@@ -0,0 +1,15 @@
+import * as K from 'kbar'
+
+export default {
+  Provider: K.KBarProvider,
+  Portal: K.KBarPortal,
+  Positioner: K.KBarPositioner,
+  Animator: K.KBarAnimator,
+  Search: K.KBarSearch,
+  createAction: K.createAction,
+  getListboxItemId: K.getListboxItemId,
+  useKBar: K.useKBar,
+  useMatches: K.useMatches,
+  useRegisterActions: K.useRegisterActions,
+  Results: K.KBarResults
+}
