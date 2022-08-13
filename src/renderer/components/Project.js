7663a599052eb1de902811ddafeb57f2ebf58ffe
dehmer <horst.dehmer@syncpoint.io>
2022-08-13 09:48:38 +0200

    kbar: improved styling.

--- src/renderer/components/Project.js
+++ src/renderer/components/Project.js
@@ -1,41 +1,14 @@
-import * as R from 'ramda'
 import React from 'react'
-import Icon from '@mdi/react'
-import * as mdi from '@mdi/js'
-import uuid from 'uuid-random'
 import { Map } from './map/Map'
 import { Properties } from './properties/Properties'
 import { CommandPalette } from './commandpalette/CommandPalette'
 import { Sidebar } from './sidebar/Sidebar'
 import { Toolbar } from './Toolbar'
-import KBar from './kbar/kbar'
+import { KBar } from './kbar/KBar'
 import { OSD } from './OSD'
 import { useServices, useMemento } from './hooks'
-import * as MILSTD from '../symbology/2525c'
-import { url } from '../symbology/symbol'
 import './Project.css'
-import './kbar/kbar.scss'
 
-function setCharAt (s, i, c) {
-  if (i > s.length - 1) return s
-  return s.substring(0, i) + c + s.substring(i + 1)
-}
-
-const actions = Object.entries(MILSTD.index)
-  .filter(([key, descriptor]) => descriptor?.geometry?.type === 'Point')
-  .map(([key, descriptor]) => {
-    const { hierarchy } = descriptor
-    const sidc = setCharAt(setCharAt(key, 1, 'F'), 3, 'P')
-    const keywords = R.uniq(hierarchy.flatMap(s => s.split(' ')))
-    return {
-      id: key,
-      name: R.last(hierarchy),
-      keywords,
-      icon: url(sidc), // FIXME: use SVG
-      perform: () => console.log('perform', sidc),
-      dryRun: () => console.log('dryRun', sidc)
-    }
-  })
 
 const handlers = {
   palette: (state, palette) => ({ ...state, palette })
@@ -47,61 +20,6 @@ const reducer = (state, event) => {
   else return state
 }
 
-const Results = () => {
-  const { results: matches } = KBar.useMatches()
-  const onRender = ({ item, active }) => {
-    // if (active) item.dryRun()
-
-    const icon = path => <Icon key={uuid()} className='ec35-key' path={mdi[path]}></Icon>
-    const span = token => <span key={uuid()} className='ec35-key'>{token}</span>
-    const separator = () => <span key={uuid()}>&nbsp;&nbsp;</span>
-
-    const avatar = item.icon
-      ? <img src={item.icon} className='ec35-image'/>
-      : item.shortcut
-        ? item.shortcut[0].split(/( )/)
-          .flatMap(token => token.split('+'))
-          .map(token => {
-            if (token === '$mod') return icon('mdiAppleKeyboardCommand')
-            else if (token === 'Control') return icon('mdiAppleKeyboardControl')
-            else if (token === 'Shift') return icon('mdiAppleKeyboardShift')
-            else if (token === 'Option') return icon('mdiAppleKeyboardOption')
-            else if (/^[A-Z]$/i.test(token)) return span(token)
-            else if (token === ' ') return separator()
-            else return token
-          })
-        : null
-
-    return (
-      <div className={active ? 'ec35-item ec35-item--active' : 'ec35-item'}>
-        <div className='ec35-item__title'>
-          {item.name}
-        </div>
-        <div className='ec35-item__avatar'>
-          {avatar}
-        </div>
-      </div>
-    )
-  }
-
-  return <KBar.Results items={matches} onRender={onRender}/>
-}
-
-const DryRunner = () => {
-  const { results } = KBar.useMatches()
-  const { visualState, activeIndex } = KBar.useKBar(R.identity)
-
-  React.useEffect(() => {
-    if (visualState !== 'showing') return
-    const active = results[activeIndex]
-    active && active.dryRun && active.dryRun()
-  }, [results, activeIndex, visualState])
-
-  React.useEffect(() => {
-    if (!['animating-in', 'animating-out'].includes(visualState)) return
-    console.log('visualState', visualState)
-  }, [visualState])
-}
 
 /**
  * <Map/> and <Workspace/> are siblings with <body/> as parent.
@@ -165,17 +83,7 @@ export const Project = () => {
           <OSD/>
           <Properties/>
         </div>
-        <KBar.Provider actions={actions}>
-          <KBar.Portal>
-            <KBar.Positioner className='ec35-positioner'>
-              <KBar.Animator className='ec35-animator'>
-                <KBar.Search className='ec35-search'/>
-                <Results/>
-                <DryRunner/>
-              </KBar.Animator>
-            </KBar.Positioner>
-          </KBar.Portal>
-        </KBar.Provider>
+        <KBar/>
       </div>
     </div>
   )
--- src/renderer/components/kbar/kbar.js
+++ src/renderer/components/kbar/kbar.js
@@ -1,18 +1,133 @@
-import * as K from 'kbar'
+import * as R from 'ramda'
+import React from 'react'
+import Icon from '@mdi/react'
+import * as mdi from '@mdi/js'
+import uuid from 'uuid-random'
+import {
+  KBarProvider as Provider,
+  KBarPortal as Portal,
+  KBarPositioner as Positioner,
+  KBarAnimator as Animator,
+  KBarSearch as Search,
+  KBarResults as Results,
+  useMatches,
+  useKBar
+} from 'kbar'
+import * as MILSTD from '../../symbology/2525c'
+import { svg } from '../../symbology/symbol'
+import './KBar.scss'
 
-console.log(K)
 
-export default {
-  Context: K.KBarContext,
-  Provider: K.KBarProvider,
-  Portal: K.KBarPortal,
-  Positioner: K.KBarPositioner,
-  Animator: K.KBarAnimator,
-  Search: K.KBarSearch,
-  createAction: K.createAction,
-  getListboxItemId: K.getListboxItemId,
-  useKBar: K.useKBar,
-  useMatches: K.useMatches,
-  useRegisterActions: K.useRegisterActions,
-  Results: K.KBarResults
+function setCharAt (s, i, c) {
+  if (i > s.length - 1) return s
+  return s.substring(0, i) + c + s.substring(i + 1)
+}
+
+const globalActions = [
+  {
+    id: 'command:create:layer',
+    name: 'Create - New Layer',
+    keywords: ['create', 'new', 'layer'],
+    shortcut: ['$mod+N L'],
+    perform: () => console.log('perform/command:create:layer'),
+    dryRun: () => {}
+  },
+  {
+    id: 'command:create:bookmark',
+    name: 'Create - New Bookmark',
+    keywords: ['create', 'new', 'bookmark'],
+    shortcut: ['$mod+N B'],
+    perform: () => console.log('perform/command:create:bookmark'),
+    dryRun: () => {}
+  }
+]
+
+const actions = Object.entries(MILSTD.index)
+  .filter(([key, descriptor]) => descriptor?.geometry?.type === 'Point')
+  .map(([key, descriptor]) => {
+    const { hierarchy } = descriptor
+    const sidc = setCharAt(setCharAt(key, 1, 'F'), 3, 'P')
+    const keywords = R.uniq(hierarchy.flatMap(s => s.split(' ')))
+    return {
+      id: key,
+      name: R.last(hierarchy),
+      keywords,
+      icon: svg(sidc),
+      perform: () => console.log('perform', sidc),
+      dryRun: () => console.log('dryRun', sidc)
+    }
+  })
+
+
+const List = () => {
+  const { results: matches } = useMatches()
+  const onRender = ({ item, active }) => {
+    // if (active) item.dryRun()
+
+    const icon = path => <Icon key={uuid()} className='ec35-key' path={mdi[path]}></Icon>
+    const span = token => <span key={uuid()} className='ec35-key'>{token}</span>
+    const separator = () => <span key={uuid()}>&nbsp;&nbsp;</span>
+
+    const avatar = item.icon
+      // ? <img src={item.icon} className='ec35-image'/>
+      ? <div className='ec35-avatar' dangerouslySetInnerHTML={{ __html: item.icon }}/>
+      : item.shortcut
+        ? item.shortcut[0].split(/( )/)
+          .flatMap(token => token.split('+'))
+          .map(token => {
+            if (token === '$mod') return icon('mdiAppleKeyboardCommand')
+            else if (token === 'Control') return icon('mdiAppleKeyboardControl')
+            else if (token === 'Shift') return icon('mdiAppleKeyboardShift')
+            else if (token === 'Option') return icon('mdiAppleKeyboardOption')
+            else if (/^[A-Z]$/i.test(token)) return span(token)
+            else if (token === ' ') return separator()
+            else return token
+          })
+        : null
+
+    return (
+      <div className={active ? 'ec35-item ec35-item--active' : 'ec35-item'}>
+        <div className='ec35-item__title'>
+          {item.name}
+        </div>
+        <div className='ec35-item__avatar'>
+          {avatar}
+        </div>
+      </div>
+    )
+  }
+
+  return <Results items={matches} onRender={onRender}/>
+}
+
+const DryRunner = () => {
+  const { results } = useMatches()
+  const { visualState, activeIndex } = useKBar(R.identity)
+
+  React.useEffect(() => {
+    if (visualState !== 'showing') return
+    const active = results[activeIndex]
+    active && active.dryRun && active.dryRun()
+  }, [results, activeIndex, visualState])
+
+  React.useEffect(() => {
+    if (!['animating-in', 'animating-out'].includes(visualState)) return
+    console.log('visualState', visualState)
+  }, [visualState])
+}
+
+export const KBar = () => {
+  return (
+    <Provider actions={[...globalActions, ...actions]}>
+      <Portal>
+        <Positioner className='ec35-positioner'>
+          <Animator className='ec35-animator'>
+            <Search className='ec35-search'/>
+            <List/>
+            <DryRunner/>
+          </Animator>
+        </Positioner>
+      </Portal>
+    </Provider>
+  )
 }
