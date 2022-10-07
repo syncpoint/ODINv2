8656688f0250706a7042e8ae33cda9594a88cbfd
Thomas Halwax <thomas.halwax@syncpoint.io>
2022-10-07 12:47:37 +0200

    dynamically showing distance/bearing/area/circumfence

--- src/renderer/store/options/measurement.js
+++ src/renderer/store/options/measurement.js
@@ -1,13 +1,31 @@
 import * as R from 'ramda'
+import { area, length } from '../../ol/interaction/measure/tools'
 import * as ID from '../../ids'
+import LineString from 'ol/geom/LineString'
+import Polygon from 'ol/geom/Polygon'
+
+const getDescription = geometry => {
+  switch (geometry.type) {
+    case 'LineString': return `Distance ${length(new LineString(geometry.coordinates))}`
+    case 'Polygon': { const polygon = new Polygon(geometry.coordinates); return `Area ${area(polygon)} - Circumfence ${length(polygon)}` }
+    default: return ''
+  }
+}
 
 export default async function (id) {
   const keys = [R.identity, ID.hiddenId, ID.lockedId, ID.tagsId]
   const [measurement, hidden, locked, tags] = await this.store.collect(id, keys)
 
+  const geometries = await this.store.geometries(id)
+  const description = geometries.length === 1
+    ? getDescription(geometries[0])
+    : undefined
+
+
   return {
     id,
-    title: measurement.name || 'MEZZUNG',
+    title: measurement.name,
+    description,
     tags: [
       'SCOPE:MEASUREMENT:NONE',
       hidden ? 'SYSTEM:HIDDEN' : 'SYSTEM:VISIBLE',
