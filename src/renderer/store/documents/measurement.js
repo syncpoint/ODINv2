c8b14024fa10606ec91e25840e8fb721bc423677
Thomas Halwax <thomas.halwax@syncpoint.io>
2022-10-07 14:13:01 +0200

    fixed access to undefined

--- src/renderer/store/documents/measurement.js
+++ src/renderer/store/documents/measurement.js
@@ -8,7 +8,7 @@ export default async function (id) {
   return {
     id,
     scope: 'measurement',
-    text: measurement.name || '',
+    text: measurement?.name || '',
     tags: tags || []
   }
 }
