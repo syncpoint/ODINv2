e2437043052f4170f189065468753973c7724401
Thomas Halwax <thomas.halwax@syncpoint.io>
2022-12-19 22:04:18 +0100

    OSX version should have an about dialog, too

--- src/main/menu/app-menu.js
+++ src/main/menu/app-menu.js
@@ -2,21 +2,29 @@ export default options => {
   const platform = options.platform || process.platform
   const appName = options.appMenu || 'ODIN'
 
-  if (platform !== 'darwin') return []
-  else {
-    return {
-      label: appName,
-      submenu: [
-        { role: 'about' },
-        { type: 'separator' },
-        { role: 'services' },
-        { type: 'separator' },
-        { role: 'hide' },
-        { role: 'hideothers' },
-        { role: 'unhide' },
-        { type: 'separator' },
-        { role: 'quit' }
-      ]
-    }
+  const menu = {
+    label: appName,
+    submenu: [
+      { role: 'about' }
+    ]
   }
+
+  if (platform !== 'darwin') {
+    menu.submenu.push(
+      { type: 'separator' },
+      { role: 'quit' }
+    )
+  } else {
+    menu.submenu.push(
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    )
+  }
+  return menu
 }
