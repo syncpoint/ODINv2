c39dba5764c7164397aa92bd2eea863bba33aec3
Thomas Halwax <thomas.halwax@syncpoint.io>
2023-09-13 16:59:38 +0200

    added tool tips for the printing toolbar

--- src/renderer/model/commands/PrintCommands.js
+++ src/renderer/model/commands/PrintCommands.js
@@ -4,18 +4,19 @@ import EventEmitter from '../../../shared/emitter'
 const state = [
   {
     event: 'TOOLBAR_SCOPE/PRINT',
-    path: 'mdiPrinterOutline'
+    path: 'mdiPrinterOutline',
+    toolTip: 'Print ...'
   },
   {
     event: 'TOOLBAR_SCOPE/STANDARD',
-    path: 'mdiExitToApp'
+    path: 'mdiExitToApp',
+    toolTip: 'Return to the main application view'
   }
 ]
 
 const PrintSwitchScopeCommand = function (services) {
   this.emitter = services.emitter
   this.currentState = 0
-  this.toolTip = 'Print'
 }
 
 Object.assign(PrintSwitchScopeCommand.prototype, EventEmitter.prototype)
@@ -31,13 +32,21 @@ PrintSwitchScopeCommand.prototype.enabled = function () { return true }
 const PrintCommand = function (services) {
   this.emitter = services.emitter
   this.path = 'mdiPrinter'
+  this.toolTip = 'Print this view now!'
 }
 
 PrintCommand.prototype.execute = function () {
   this.emitter.emit('PRINT')
 }
 
+const buildScopeCommand = (services) => {
+  const command = new PrintSwitchScopeCommand(services)
+  Object.defineProperty(command, 'path', { get () { return state[this.currentState].path } })
+  Object.defineProperty(command, 'toolTip', { get () { return state[this.currentState].toolTip } })
+  return command
+}
+
 export default services => ({
-  PRINT_SWITCH_SCOPE: Object.defineProperty(new PrintSwitchScopeCommand(services), 'path', { get () { return state[this.currentState].path } }),
+  PRINT_SWITCH_SCOPE: buildScopeCommand(services),
   PRINT_MAP: new PrintCommand(services)
 })
