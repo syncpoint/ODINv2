4f6e6b57cf58b956a0582f2642d0a926f03d0b35
Thomas Halwax <thomas.halwax@syncpoint.io>
2024-02-07 17:40:16 +0100

    restrict links if containers are restricted

--- src/renderer/store/documents/link.js
+++ src/renderer/store/documents/link.js
@@ -2,13 +2,16 @@ import * as R from 'ramda'
 import * as ID from '../../ids'
 
 export default async function (id) {
-  const keys = [R.identity, ID.tagsId]
-  const [link, tags] = await this.store.collect(id, keys)
+  const keys = [R.identity, ID.tagsId, ID.restrictedId]
+  const [link, tags, restricted] = await this.store.collect(id, keys)
 
   return {
     id,
     scope: ID.LINK,
     text: link.name,
-    tags: tags || []
+    tags: [
+      restricted ? 'restricted' : '',
+      ...(tags || [])
+    ]
   }
 }
