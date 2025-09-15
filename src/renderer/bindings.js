d75e90fb0adeca839f72fdb2892871917790c85d
dehmer <horst.dehmer@gmail.com>
2025-09-15 20:13:16 +0200

    clean-up.

--- src/renderer/bindings.js
+++ src/renderer/bindings.js
@@ -6,12 +6,16 @@ const cmdOrCtrl = key => process.platform === 'darwin'
   : `ctrl+${key}`
 
 export const bindings = (commandRegistry, emitter) => {
-
   const [/* unused */, deleteCommand] = commandRegistry.command('CLIPBOARD_DELETE')
 
-
   // Note: bindGlobal() is used to also trap inside input elements.
-  Mousetrap.bind(cmdOrCtrl('backspace'), () => { console.log('CMD+CTRL+Backspace key binding'); if (deleteCommand.enabled()) deleteCommand.execute() })
-  Mousetrap.bind('del', () => { console.log('DEL key binding'); if (deleteCommand.enabled()) deleteCommand.execute() })
+  Mousetrap.bind(cmdOrCtrl('backspace'), () => {
+    if (deleteCommand.enabled()) deleteCommand.execute()
+  })
+
+  Mousetrap.bind('del', () => {
+    if (deleteCommand.enabled()) deleteCommand.execute()
+  })
+
   Mousetrap.bind('esc', () => emitter.emit('command/draw/cancel'))
 }
